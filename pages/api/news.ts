5dda613d59bbf2d4f940c2a5200f9ae764dc273a
Hamza Jassar <jassarhamza@gmail.com>
2023-03-19 10:25:23 -0500

    looks good enough

--- pages/api/news.ts
+++ pages/api/news.ts
@@ -9,19 +9,14 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
-  const { category_slug, featured } = req.query;
-
   //  The iffy part is the filterByFormula.
   const comma = "%2C";
-  const filters = [];
-  if (category_slug)
-    filters.push(encodeURIComponent(`category_slug="${category_slug}"`));
-  if (featured) filters.push(encodeURIComponent(`featured`));
+  const filters: string[] = [];
   let filter = `filterByFormula=AND(${filters.join(comma)})`;
   // Sorting is also a bit iffy.
-  const sortField = "";
-  // encodeURIComponent("sort[0][field]") + `=${encodeURIComponent("order")}`;
-  const sortDirection = encodeURIComponent("sort[0][direction]") + `=asc`;
+  const sortField =
+    encodeURIComponent("sort[0][field]") + `=${encodeURIComponent("Date")}`;
+  const sortDirection = encodeURIComponent("sort[0][direction]") + `=desc`;
   // One big url!
   const url = `${api}/${base}/${table}?${filter}&${sortField}&${sortDirection}`;
 
