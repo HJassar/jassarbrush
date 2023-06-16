0c35283b5ef8e5c433c1d3e0c0fc838aff7a48e0
Hamza Jassar <jassarhamza@gmail.com>
2023-06-16 11:33:13 +0300

    hopefully a winner! David Shepherd Award

--- data/awards.ts
+++ data/awards.ts
@@ -1,4 +1,5 @@
 export default [
+  "2023 David Shepherd Wildlife Foundation (DSWF) Wildlife Artist of the Year - Finalist",
   '2021 richeson75 "Animals, Birds & Wildlife 2021" – Meritorious List',
   '2021 Art Show International "Water" Talent Prize Award – Winner',
   "2016 World Illustration Challenge by Grafolio – Winner",
