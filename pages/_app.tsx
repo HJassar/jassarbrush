520faaaedfba35cdf2a753809c5367295a532f17
Hamza Jassar <jassarhamza@gmail.com>
2023-07-29 09:29:35 +0300

    added vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,14 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { Analytics } from "@vercel/analytics/react";
+
 import { LBProvider } from "../LBContext";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <LBProvider>
       <Component {...pageProps} />
+      <Analytics />
     </LBProvider>
   );
 }
