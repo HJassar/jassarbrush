35541185e9d6254e53c73f8a89ffe27b88a720e1
Hamza Jassar <jassarhamza@gmail.com>
2024-04-10 22:01:14 +0300

    added murals

--- data/categories.ts
+++ data/categories.ts
@@ -1,5 +1,6 @@
 export const categories = [
   { slug: "fine-art", name: "Fine Art" },
+  { slug: "murals", name: "Murals" },
   { slug: "illustrations", name: "Illustrations" },
   { slug: "caricatures", name: "Caricatures" },
 ];
